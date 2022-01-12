6f214d26b079c32df902aa1e4e1bdee02f204240
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 06:27:10 +0700

    Update and rename utils/dbDriver.js to driver.js

--- driver.js
+++ driver.js
@@ -1,5 +1,6 @@
-var {URI,USER,PASSWORD} = process.env
 var {driver,auth}  = require('neo4j-driver')
+var {URI,USER,PASSWORD} = process.env
+
 
 var _driver = driver(URI,auth.basic(
     USER,PASSWORD
