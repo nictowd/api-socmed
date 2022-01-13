b3957fc910f3fb842d861de5d1ef3c255fa246d5
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 08:50:49 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,23 @@ var Session = require('../utils/session')
 var driver = require('../utils/driver')
 var dbDriver = driver(process.env);
 var express = require('express');
-var session = Session(dbDriver);
+var sessions = Session(dbDriver);
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var newSession = session.create(
-    session.driver
-  )
-  res.send(newSession)
+  try{
+    var session = sessions.create(
+      sessions.driver
+    )
+    var result = await session.run(
+      'match(n) return n'
+    )
+    res.send(result)
+  }
+  catch({message}){
+    res.send(message)
+  }
 });
 
 module.exports = router;
