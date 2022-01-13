44282f9b8cf16889f459c216dbd738350c3e2adc
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 07:29:56 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(`${process.env.dbPassword}`)
+  res.send(process.env)
 });
 
 module.exports = router;
