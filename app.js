41801424a01ecce0f34ab4b6f9c3d6ef076ff1c3
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 13:56:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ var logger = require('morgan');
 
 var userRouter = require('./routes/user');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+var app = express()
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,13 +23,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send(
+    `${err.status}:${err.message}`
+  )
 });
 
 module.exports = app;
