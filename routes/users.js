459cc745422d1a68dab4ef63804fc2042a596b33
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 13:49:28 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
+var Session = require('../utils/session')
+var driver = require('../utils/driver')
+var dbDriver = driver(process.env);
 var express = require('express');
+var sessions = Session(dbDriver);
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get('/:id', async(req,res) => {
+  try{
+    var session = sessions.create(
+      sessions.driver
+    )
+    var result = await session.run(
+      `match(user:user{id: $id})
+       return user`,req.params
+    )
+    res.send(result)
+  }
+  catch({message}){
+    res.send(message)
+  }
 });
 
 module.exports = router;
