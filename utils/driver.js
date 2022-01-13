5c0e132379f848ba1045b99de16015983a9a7e91
nictowd <97575840+nictowd@users.noreply.github.com>
2022-01-13 07:44:21 +0700

    Update driver.js

--- utils/driver.js
+++ utils/driver.js
@@ -1,7 +1,7 @@
 var {driver,auth} = require('neo4j-driver')
 
 module.exports = ({URI,USER,PASSWORD}) => {
-  return driver(URI,AUTH.BASIC(
+  return driver(URI,auth.basic(
     USER,PASSWORD
   ))
 }
